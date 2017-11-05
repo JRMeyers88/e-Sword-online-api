4cb4ddc8034b31f5bef87b4600be3a5cb6c52190
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-11-05 13:58:00 -0600

    select and get commentary by book along with bible

--- app/controllers/commentaryCtrl.js
+++ app/controllers/commentaryCtrl.js
@@ -2,8 +2,8 @@
 
 const { getBookCommentary } = require('../models/commentaries');
 
-module.exports.getABookCommentary = (req, res, next) => {
-  getBookCommentary()
+module.exports.getABookCommentary = ({params:{commentary, book}}, res, next) => {
+  getBookCommentary(commentary, book)
   .then( (data) => {
     res.status(200).json(data);
   })
--- routes/commentaries.js
+++ routes/commentaries.js
@@ -4,6 +4,6 @@ const { Router } = require('express');
 const router = Router();
 const { getABookCommentary } = require('../app/controllers/commentaryCtrl');
 
-router.get('/commentary', getABookCommentary);
+router.get('/comments/:commentary/:book', getABookCommentary);
 
 module.exports = router;
